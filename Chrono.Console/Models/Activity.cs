fe1cda64ca1a15eabc1b4fcba67f33b739dee5d2
Gabriel <gmessiasp@gmail.com>
2024-12-03 18:30:55 -0300

    Trocando strings de opções para enums

--- Chrono.Console/Models/Activity.cs
+++ Chrono.Console/Models/Activity.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Chrono.Console.Models;
 
 public sealed class Activity
 {
     public long Id { get; init ; }
+    [MaxLength(16)]
     public string Name { get; set; } = string.Empty;
     public bool IsActive { get; set; } = true;
 }
